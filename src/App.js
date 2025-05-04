57c01a8c1cb15e83c6cf22c04896c7133fe46220
George551556 <2869948036@qq.com>
2025-05-04 19:10:37 +0800

    完成工人下发及执行，工人发送设备状态

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import {
   Ping,
   queryServerClock,
   queryDevices,
+  startTaskAll,
+  switchDeviceAll,
 } from './service/service';
 import { Input, Button, Select, Space, message, Modal, Col, Row, Spin, } from 'antd';
 import { ClockCircleOutlined } from '@ant-design/icons';
 
 const { TextArea } = Input;
 function App() {
-  const [serverClock, setServerClock] = useState('xx:xx:xx');
+  const [serverClock, setServerClock] = useState('无连接');
   const [devices, setDevices] = useState([]);
   const [deviceNums, setDeviceNums] = useState(0);
+  const [requestBashText, setRequestBashText] = useState('');
+  const [totalRequestNums, setTotalRequestNums] = useState(1000);
+  const [usingThreadsNums, setUsingThreadsNums] = useState(5);
+  const [timeConstraint, setTimeConstraint] = useState(2);
   
 
   // 页面加载后获取数据等操作
@@ -28,15 +34,18 @@ function App() {
       queryServerClock().then(res => {
         setServerClock(res.data.message);
       }).catch(err => {
+        setServerClock('无连接');
         console.log(err);
       });
     }, 2000);
 
-    // 获取设备列表
+    // 获取设备列表 TODO: 设置为定时刷新，记得清除定时器
     queryDevices().then(res => {
       setDevices(res.data.data);
       setDeviceNums(res.data.deviceNums);
-    })
+    }).catch(err => {
+      console.log(err);
+    });
 
     return () => {
       // 组件销毁时清除定时器
@@ -44,33 +53,54 @@ function App() {
     }
   }, [])
 
+  // 提交表单
+  const handleStartTaskAll = () => {
+    message.info('请求发送中...')
+    startTaskAll(requestBashText, totalRequestNums, usingThreadsNums, timeConstraint).then(res => {
+      message.success('请求发送成功');
+    }).catch(err => {
+      message.error('请求发送失败');
+      console.log(err);
+    });
+  }
+
+  // 重置表单
+  const handleBtnClear = () => {
+    // setRequestBashText('');
+    setTotalRequestNums(1000);
+    setUsingThreadsNums(5);
+    setTimeConstraint(2);
+  }
+
   return (
     <div className="App">
       <div style={{height: '10px'}}></div>
         <Space direction='horizontal'>
           {/* 顶部控制栏 */}
           <Input value={serverClock} prefix={<ClockCircleOutlined />} className='Top-clock'/>
-          <Button type="primary" size='large'>Primary</Button>
-          <Button size='large'>Default</Button>
+          <Button type="primary" size='large' onClick={() => {switchDeviceAll(true, '')}}>启动所有设备</Button>
+          <Button size='large' onClick={() => {switchDeviceAll(false, '')}}>停止所有设备</Button>
         </Space>
         <div style={{height: '50px'}}></div>
         <Row>
           {/* 左下 控制表单 */}
           <Col span={1}></Col>
-          <Col span={8} style={{}}>
+          <Col span={9} style={{}}>
             <div className='Left_bottom_container'>
-              <TextArea rows={6} placeholder='request url and body (bash)' />
-              <Input placeholder='total request nums' />
-              <Input placeholder='parallel requests'/>
-              <Input placeholder='time constraint'/>
-              <Button >按钮</Button>
-              <Button >按钮</Button>
+              <TextArea rows={14} placeholder='request url and body (bash)' value={requestBashText} onChange={(e) => {setRequestBashText(e.target.value)}} allowClear />
+              <Input prefix='总请求数量: ' value={totalRequestNums} onChange={(e) => {setTotalRequestNums(e.target.value)}} type='number' />
+              <Input prefix='并发请求数: ' value={usingThreadsNums} onChange={(e) => {setUsingThreadsNums(e.target.value)}} type='number' />
+              <Input prefix='时间限制: ' value={timeConstraint} onChange={(e) => {setTimeConstraint(e.target.value)}} suffix='分钟' type='number' />
+              <Space direction='horizontal'>
+                <Button type='primary' onClick={handleStartTaskAll} >提交</Button>
+                <Button onClick={handleBtnClear} >重置</Button>
+              </Space>
 
             </div>
           </Col>
 
           {/* 右下 设备列表 */}
-          <Col span={14} style={{}}>
+          <Col span={13} style={{}}>
             <div>
               <DeviceList data={devices} deviceNums={deviceNums} />
             </div>
--- src/service/service.js
+++ src/service/service.js
@@ -11,3 +11,20 @@ export const queryServerClock  = async () => {
 export const queryDevices = async () => {
     return await axios.get('/front/queryDevices');
 }
+
+export const startTaskAll = async (requestBashText, totalRequestNums, usingThreadsNums, timeConstraint) => {
+    return await axios.post('/front/startTaskAll', {
+        request_bash_text: requestBashText,
+        enable_random_params: [],
+        total_request_nums: Number(totalRequestNums),
+        using_threads_nums: Number(usingThreadsNums),
+        time_constraint: Number(timeConstraint),
+    });
+}
+
+export const switchDeviceAll = async (isWorking, deviceID) => {
+    return await axios.post('/front/switchDeviceAll', {
+        is_working: isWorking,
+        device_id: deviceID
+    });
+}
